82df433707fb6f803738441c78a9c33f4b2ad9d9
Trueshka <truepbtrue@mail.ru>
2020-09-23 14:06:38 +0300

    HW0 error correction

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -1,12 +1,13 @@
 package ru.javawebinar.topjava.util;
 
+
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -22,20 +23,19 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
         );
 
-        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2400);
+        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsTo.forEach(System.out::println);
         System.out.println("___________________");
-        List<UserMealWithExcess> mealsTo2 = filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2400);
+        List<UserMealWithExcess> mealsTo2 = filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
         mealsTo2.forEach(System.out::println);
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         List<UserMealWithExcess> resultMeals = new ArrayList<>();
-        Map<String, Integer> map = findDayCalories(meals);
-        for (UserMeal meal : meals)
-        {
+        Map<LocalDate, Integer> findCalories = findDayCalories(meals);
+        for (UserMeal meal : meals) {
             if (TimeUtil.isBetweenHalfOpen(meal.getDateTime().toLocalTime(), startTime, endTime)) {
-                boolean excess = map.get(meal.getDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))) < caloriesPerDay;
+                boolean excess = findCalories.get(meal.getDateTime().toLocalDate()) > caloriesPerDay;
                 resultMeals.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), excess));
             }
         }
@@ -43,22 +43,23 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        Map<String, Integer> map = findDayCalories(meals);
+        Map<LocalDate, Integer> daytimeCalories = meals.stream().collect(
+                Collectors.groupingBy(meal -> meal.getDateTime().toLocalDate(),
+                        Collectors.summingInt(UserMeal::getCalories))
+        );
         return meals.stream()
                 .filter(x -> TimeUtil.isBetweenHalfOpen(x.getDateTime().toLocalTime(), startTime, endTime))
                 .map(x -> new UserMealWithExcess(x.getDateTime(), x.getDescription(), x.getCalories(),
-                        map.get(x.getDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))) < caloriesPerDay))
+                        daytimeCalories.get(x.getDateTime().toLocalDate()) > caloriesPerDay))
                 .collect(Collectors.toList());
     }
 
-    public static Map<String, Integer> findDayCalories(List<UserMeal> meals) {
-        Map<String, Integer> map = new HashMap<>();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        for (UserMeal meal : meals)
-        {
-            map.putIfAbsent(meal.getDateTime().format(formatter), meal.getCalories());
-            map.computeIfPresent(meal.getDateTime().format(formatter), (key, value) -> value + meal.getCalories());
+    public static Map<LocalDate, Integer> findDayCalories(List<UserMeal> meals) {
+        Map<LocalDate, Integer> findCalories = new HashMap<>();
+        for (UserMeal meal : meals) {
+            findCalories.computeIfPresent(meal.getDateTime().toLocalDate(), (d, c) -> c + meal.getCalories());
+            findCalories.putIfAbsent(meal.getDateTime().toLocalDate(), meal.getCalories());
         }
-        return map;
+        return findCalories;
     }
 }
