0e4e55a26ea126c2d75cc078caf59c2799311f5c
Trueshka <truepbtrue@mail.ru>
2020-10-28 12:05:03 +0300

    HW4 Fix code quality

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -38,7 +38,8 @@ public class Meal extends AbstractBaseEntity {
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id")
+    @NotNull
     private User user;
 
     public Meal() {
